c7ec5e9175feb37a14326ccd99c3c1053d4b6fc7
Ayah Atheel <ayah.atheel@gmail.com>
2024-02-12 12:35:04 +0300

    1st Answer

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function showYoungestStudent(students) { }
+
+var students = [
+    { name: "Ayah", age: 26 },
+    { name: "Noor", age: 21 },
+    { name: "Sarah", age: 30 },
+    { name: "Mais", age: 24 }
+];
+
+function showYoungestStudent(students) {
+    var youngestAge = Math.min(...students.map(student => student.age));
+    var youngestStudent = students.find(student => student.age === youngestAge);
+    console.log("The youngest student is: " + youngestStudent.name);
+}
+
+showYoungestStudent(students);
+
+
+/* I found the solution to my code on Stack Overflow (Math.min...) and Developer Mozilla (.find). 
+The specific threads where I found the solution are: */
+
+console.log("https://stackoverflow.com/questions/8864430/compare-javascript-array-of-objects-to-get-min-max");
+// answer by JuZDePeche
+
+console.log("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find");
+
 
 module.exports = showYoungestStudent;
